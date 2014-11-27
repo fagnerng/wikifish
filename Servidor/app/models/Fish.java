f611e2147be7d02ec1bd3092c525e13ce408c397
felipeefb <malkavianbr@gmail.com>
2014-11-27 17:23:42 -0300

    refatorando classe peixe
    Adicionado ENUMS para os atributos que apenas um entre eles poderiam ser setados.

--- Servidor/app/models/Fish.java
+++ Servidor/app/models/Fish.java
@@ -14,6 +14,24 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @JsonInclude(NON_EMPTY)
 public class Fish extends Model {
+	
+	private enum ReproductionEnum {
+		EGGYLAYER, LIVEBEARER, MOUTHBROODER
+	}
+	private enum AquariumLightEnum{
+		BRIGHT, MEDIUM, DARK
+	}
+	private enum TemperamentEnum {
+		PEACEFUL, AGRESSIVE
+	}
+	private enum AquariumSetUpEnum {
+		DENSIDLY_PLANTED, ONLY_ROCKS, ONLY_SUBSTRACT, COMPLETED_SET		
+	}
+	private enum SwimmingEnum {
+		BOTTOM, TOP, MIDDLE, NO_SPECIAL
+	}
+	
+	
 
     @Id
     private long id;
@@ -21,7 +39,7 @@ public class Fish extends Model {
     private String usualName;
     private String cientificName;
     private float ph;
-    private float dh;
+    private int dh;
     private float temperature;
     private float maximumLength;
     private float aquarium_liters;
@@ -29,247 +47,21 @@ public class Fish extends Model {
     private boolean live_worms;
     private boolean live_fish;
     private boolean vegetarian;
-    private boolean peaceful;
-    private boolean agressive;
-    private boolean eggylayer;
-    private boolean livebearer;
-    private boolean mothbrooder;
-    private boolean bottom;
-    private boolean top;
-    private boolean middle;
-    private boolean no_special;
-    private boolean densidly_plated;
-    private boolean only_rocks;
-    private boolean only_substract;
-    private boolean complete_set;
-    private boolean bright;
-    private boolean medium;
-    private boolean dark;
+	private ReproductionENum reproduction;
+	private AquariumLightEnum aquariumLight;
+	private TemperamentEnum temperament;
+	private AquariumSetUpEnum AquariumSetUp;
+	private SwimmingEnum swimming;	
 
     @OneToMany(cascade = ALL, fetch = LAZY)
     private List<Comment> comments;
 
     @OneToOne(cascade = ALL)
     private Region region;
-
-    public float getAquarium_liters() {
-        return aquarium_liters;
-    }
-
-    public void setAquarium_liters(float aquarium_liters) {
-        this.aquarium_liters = aquarium_liters;
-    }
-
-    public boolean isDry_package_food() {
-        return dry_package_food;
-    }
-
-    public void setDry_package_food(boolean dry_package_food) {
-        this.dry_package_food = dry_package_food;
-    }
-
-    public boolean isLive_worms() {
-        return live_worms;
-    }
-
-    public void setLive_worms(boolean live_worms) {
-        this.live_worms = live_worms;
-    }
-
-    public boolean isLive_fish() {
-        return live_fish;
-    }
-
-    public void setLive_fish(boolean live_fish) {
-        this.live_fish = live_fish;
-    }
-
-    public boolean isVegetarian() {
-        return vegetarian;
-    }
-
-    public void setVegetarian(boolean vegetarian) {
-        this.vegetarian = vegetarian;
-    }
-
-    public boolean isPeaceful() {
-        return peaceful;
-    }
-
-    public void setPeaceful(boolean peaceful) {
-        this.peaceful = peaceful;
-    }
-
-    public boolean isAgressive() {
-        return agressive;
-    }
-
-    public void setAgressive(boolean agressive) {
-        this.agressive = agressive;
-    }
-
-    public boolean isEggylayer() {
-        return eggylayer;
-    }
-
-    public void setEggylayer(boolean eggylayer) {
-        this.eggylayer = eggylayer;
-    }
-
-    public boolean isLivebearer() {
-        return livebearer;
-    }
-
-    public void setLivebearer(boolean livebearer) {
-        this.livebearer = livebearer;
-    }
-
-    public boolean isMothbrooder() {
-        return mothbrooder;
-    }
-
-    public void setMothbrooder(boolean mothbrooder) {
-        this.mothbrooder = mothbrooder;
-    }
-
-    public boolean isBottom() {
-        return bottom;
-    }
-
-    public void setBottom(boolean bottom) {
-        this.bottom = bottom;
-    }
-
-    public boolean isTop() {
-        return top;
-    }
-
-    public void setTop(boolean top) {
-        this.top = top;
-    }
-
-    public boolean isMiddle() {
-        return middle;
-    }
-
-    public void setMiddle(boolean middle) {
-        this.middle = middle;
-    }
-
-    public boolean isNo_special() {
-        return no_special;
-    }
-
-    public void setNo_special(boolean no_special) {
-        this.no_special = no_special;
-    }
-
-    public boolean isDensidly_plated() {
-        return densidly_plated;
-    }
-
-    public void setDensidly_plated(boolean densidly_plated) {
-        this.densidly_plated = densidly_plated;
-    }
-
-    public boolean isOnly_rocks() {
-        return only_rocks;
-    }
-
-    public void setOnly_rocks(boolean only_rocks) {
-        this.only_rocks = only_rocks;
-    }
-
-    public boolean isOnly_substract() {
-        return only_substract;
-    }
-
-    public void setOnly_substract(boolean only_substract) {
-        this.only_substract = only_substract;
-    }
-
-    public boolean isComplete_set() {
-        return complete_set;
-    }
-
-    public void setComplete_set(boolean complete_set) {
-        this.complete_set = complete_set;
-    }
-
-    public boolean isBright() {
-        return bright;
-    }
-
-    public void setBright(boolean bright) {
-        this.bright = bright;
-    }
-
-    public boolean isMedium() {
-        return medium;
-    }
-
-    public void setMedium(boolean medium) {
-        this.medium = medium;
-    }
-
-    public boolean isDark() {
-        return dark;
-    }
-
-    public void setDark(boolean dark) {
-        this.dark = dark;
-    }
-
-    public Fish() {
-        comments = new ArrayList<>();
-    }
-
-    public static final Finder<Long, Fish> FINDER = new Finder(Long.class, Fish.class);
-
-    public float getMaximumLength() {
-        return maximumLength;
-    }
-
-    public void setMaximumLength(float maximumLength) {
-        this.maximumLength = maximumLength;
-    }
-
-    public float getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(float temperature) {
-        this.temperature = temperature;
-    }
-
-    public float getDh() {
-        return dh;
-    }
-
-    public void setDh(float dh) {
-        this.dh = dh;
-    }
-
-    public float getPh() {
-        return ph;
-    }
-
-    public void setPh(float ph) {
-        this.ph = ph;
-    }
-
-    public String getCientificName() {
-        return cientificName;
-    }
-
-    public void setCientificName(String cientificName) {
-        this.cientificName = cientificName;
-    }
-
-    public long getId() {
+	
+	public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
@@ -277,28 +69,171 @@ public class Fish extends Model {
     public String getUsualName() {
         return usualName;
     }
-
     public void setUsualName(String usualName) {
         this.usualName = usualName;
     }
+	
+	 public String getCientificName() {
+        return cientificName;
+    }
+    public void setCientificName(String cientificName) {
+        this.cientificName = cientificName;
+    }
+	
+	 public float getPh() {
+        return ph;
+    }
+    public void setPh(float ph) {
+        this.ph = ph;
+    }
+	
+	public int getDh() {
+        return dh;
+    }
+    public void setDh(int dh) {
+        this.dh = dh;
+    }
+	
+	public float getTemperature() {
+        return temperature;
+    }
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+	
+	public float getMaximumLength() {
+        return maximumLength;
+    }
+    public void setMaximumLength(float maximumLength) {
+        this.maximumLength = maximumLength;
+    }
+	
+	public float getAquarium_liters() {
+        return aquarium_liters;
+    }
+    public void setAquarium_liters(float aquarium_liters) {
+        this.aquarium_liters = aquarium_liters;
+    }
+	 
+	public boolean isDry_package_food() {
+        return dry_package_food;
+    }
+    public void setDry_package_food(boolean dry_package_food) {
+        this.dry_package_food = dry_package_food;
+    }
+	
+	public boolean isLive_worms() {
+        return live_worms;
+    }
+    public void setLive_worms(boolean live_worms) {
+        this.live_worms = live_worms;
+    }
+	
+	public boolean isLive_fish() {
+        return live_fish;
+    }
+    public void setLive_fish(boolean live_fish) {
+        this.live_fish = live_fish;
+    }
 
-    public Region getRegion() {
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+   	public ReproductionEnum getReproduction(){
+	 return this.reproduction
+	}	
+	public void setReproduction(ReproductionEnum reproduction){
+		this.reproduction = reproduction;
+	}
+	
+	public AquariumLightEnum getAquariumLight(){
+	 return this.aquariumLight
+	}	
+	public void setAquariumLight(AquariumLightEnum aquariumLight){
+		this.aquariumLight = aquariumLight;
+	}
+	
+	public TemperamentEnum getTemperament(){
+		return this.temperament;
+	}	
+	public void setTemperament(TemperamentEnum temperament){
+		this.temperament = temperament;
+	}
+	
+	public AquariumSetUpEnum getAquariumSetUp(){
+		return this.aquariumSetUp;
+	}
+	public void setAquariumSetUp(AquariumSetUpEnum aquariumSetUp){
+		this.aquariumSetUp = aquariumSetUp;
+	}
+	
+	public SwimmingEnum getSwimming(){
+		return this.swimming;
+	}
+	public void setSwimming( SwimmingEnum swimming){
+		this.swimming = swimming;
+	}
+
+	public Region getRegion() {
         return region;
     }
-
     public void setRegion(Region region) {
         this.region = region;
-    }
+    }   
 
-    public List<Comment> getComments() {
+	public List<Comment> getComments() {
         return comments;
     }
-
     public void setComments(List<Comment> comments) {
         this.comments = comments;
     }
-
     public void addComment(Comment comment) {
         this.comments.add(comment);
     }
+	
+    public Fish() {
+        comments = new ArrayList<>();
+    }
+	
+	public Fish(String usualName, String cientificName) {
+		this.usualName = UsualName;
+		this.cientificName = cientificName;
+		this.ph = 7.0;
+		this.dh = 160;
+		this.temperature = 37.0;
+		this.maximumLenght = 15.0;
+		this.aquarium_liters = 100.0;
+		this.dry_package_food = true;
+		this.live_worms = false;
+		this.live_fish = false;
+		this.vegetarian = false;
+		this.reproduction = ReproductionEnum.EGGLAYER;
+		this.aquariumLight = AquariumLightEnum.MEDIUM;
+		this.temperament = TemperamentEnum.PEACEFUL;
+		this.aquariumSetUp = AquariumSetUpEnum.COMPLETED_SET;
+		this.swimming = SwimmingEnum.NO_SPECIAL;
+		comments = new ArrayList<>();
+    }
+
+    public static final Finder<Long, Fish> FINDER = new Finder(Long.class, Fish.class);
+
+  
+
+    
+
+    
+
+   
+
+   
+
+    
+
+   
+
+   
 }
